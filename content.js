200c86d604e4fb083e11e2bc40554da94c76e0fd
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2025-02-21 09:10:22 +0530

    added js logic

---
+++ content.js
@@ -0,0 +1,11 @@
+document.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+  
+  document.addEventListener('drop', (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text/plain');
+    if (data && e.target.tagName === 'INPUT' || e.target.tagName === 'TEXTAREA') {
+      e.target.value += data;
+    }
+  });
